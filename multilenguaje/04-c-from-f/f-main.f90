8b52bc644ed461e053f241b6757de98e9b83f2f7
Alexis Jawtuschenko <alexis@localhost.localdomain>
2017-03-05 12:14:07 -0300

    hecho el 04. puse n en vez de 200 en el cod. fortran. estan las reglas.txt y la ayuda que conseguimos en internet

--- multilenguaje/04-c-from-f/f-main.f90
+++ multilenguaje/04-c-from-f/f-main.f90
@@ -1,13 +1,24 @@
 program testsum
-  implicit none
+	use iso_c_binding
+	implicit none
+	interface
+		integer (c_int) function sum_abs(data,n) bind(C, name="sum_abs")
+			use iso_c_binding
+			implicit none
+			type (c_ptr), value::data
+			integer (c_int), value::n
+		end function sum_abs
+	end interface
 
-  integer, parameter :: n=200
-  integer :: data(n), asum, i
+	integer (c_int), parameter 	:: n = 200
+	integer (c_int) 			:: asum
+	integer (c_int) 			:: i
+	integer (c_int), target 	:: data(n)
 
-  do i=1,200
-    data(i) = i-100
-  end do
+	do i=1,n
+		data(i) = i-100
+	end do
 
-  call sum_abs(data,n,asum)
-  print*, 'sum=',asum
+	asum = sum_abs(c_loc(data),n)
+	print*, 'sum=',asum
 end program testsum 
