d81c7b68dd1c15bfe900ffff6572decd541e3b84
Alexis Jawtuschenko <alexis@localhost.localdomain>
2017-03-05 15:19:07 -0300

    06 hecho y reglas.txt

--- multilenguaje/06-cpp-from-c/cpp-sum.cpp
+++ multilenguaje/06-cpp-from-c/cpp-sum.cpp
@@ -1,11 +1,17 @@
 #include <cstdlib>
 #include "cpp-sum.h"
 
-int sum_abs(const int * const in, const int num) {
-   int sum=0;
-   for (int i=0; i < num; ++i) {
-       sum += abs(in[i]);
-   }
-   return sum;
+namespace Math {
+	extern "C" {
+		int sum_abs(const int * const in, const int num); 
+	}
+};
+
+int Math::sum_abs (const int * const in, const int num)
+{
+	   int sum=0;
+	   for (int i=0; i < num; ++i) {
+	       sum += abs(in[i]);
+	   }
+	   return sum;
 }
- 
--- multilenguaje/06-cpp-from-c/cpp-sum.h
+++ multilenguaje/06-cpp-from-c/cpp-sum.h
@@ -1,7 +1,4 @@
-
 #ifndef C_SUM_H
 #define C_SUM_H
-
-int sum_abs(const int * const in, const int num);
-
+extern int sum_abs(const int * const in, const int num);
 #endif
